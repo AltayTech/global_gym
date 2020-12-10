675a6d0acebd71f4efe02b8495af7fd6c420e0f0
arashsd <arashsd.2020@gmail.com>
2020-12-10 21:14:50 +0330

    change icon an some UI polish

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -108,13 +108,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
 
-    var htmlcode ="""
-    <p dir="rtl">جواب <math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>&#1784;</mn></msqrt></math> چیست؟</p>
-    """;
-    var htmlcode2 ="""
-    <p>جواب سوال&nbsp;<span class="MathJax_Preview" style="color: inherit; display: none;"></span><span class="MathJax" id="MathJax-Element-1-Frame" tabindex="0" style="position: relative;" data-mathml="<math xmlns=&quot;http://www.w3.org/1998/Math/MathML&quot;><mroot><mrow><mn>&amp;#x6F2;&amp;#x6F5;</mn><mo>&amp;#xA0;</mo><mi>x</mi></mrow><mn>&amp;#x6F3;</mn></mroot></math>" role="presentation"><nobr aria-hidden="true"><span class="math" id="MathJax-Span-1" style="display: inline-block; width: 3.235em;"><span style="display: inline-block; position: relative; width: 2.718em; height: 0px; font-size: 119%;"><span style="position: absolute; clip: rect(1.49em, 1002.72em, 3.106em, -999.997em); top: -2.582em; left: 0em;"><span class="mrow" id="MathJax-Span-2" style="display: inline-block;"><span class="mroot" id="MathJax-Span-3" style="display: inline-block;"><span style="display: inline-block; position: relative; width: 2.718em; height: 0px;"><span style="position: absolute; clip: rect(1.619em, 1001.68em, 2.977em, -999.997em); top: -2.582em; left: 0.973em;"><span class="mrow" id="MathJax-Span-4" style="display: inline-block;"><span class="mn" id="MathJax-Span-5" style="display: inline-block;"><span style="font-family: STIXGeneral, &quot;Arial Unicode MS&quot;, serif; font-size: 84%; font-style: normal; font-weight: normal;">۲</span><span style="font-family: STIXGeneral, &quot;Arial Unicode MS&quot;, serif; font-size: 84%; font-style: normal; font-weight: normal;">۵</span></span><span class="mo" id="MathJax-Span-6" style="display: inline-block;"><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Main/Regular/336/00A0.png?V=2.7.0" style="width: 0px; height: 0px; margin-right: 0.262em;"></span><span class="mi" id="MathJax-Span-7" style="display: inline-block;"><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Math/Italic/336/0078.png?V=2.7.0" style="width: 8px; height: 7px; vertical-align: 0px; margin-right: 0.068em;"></span></span><span style="display: inline-block; width: 0px; height: 2.589em;"></span></span><span style="position: absolute; clip: rect(3.559em, 1001.75em, 3.946em, -999.997em); top: -4.586em; left: 0.973em;"><span style="display: inline-block; position: relative; width: 1.749em; height: 0px;"><span style="position: absolute; top: -4.005em; left: -0.061em;"><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Main/Regular/336/2212.png?V=2.7.0" style="width: 11px; height: 1px; vertical-align: 3px; margin-right: 0.068em;"><span style="display: inline-block; width: 0px; height: 4.011em;"></span></span><span style="position: absolute; top: -4.005em; left: 1.102em;"><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Main/Regular/336/2212.png?V=2.7.0" style="width: 11px; height: 1px; vertical-align: 3px; margin-right: 0.068em;"><span style="display: inline-block; width: 0px; height: 4.011em;"></span></span><span style="position: absolute; top: -4.005em; left: 0.456em;"><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Main/Regular/336/2212.png?V=2.7.0" style="width: 11px; height: 1px; vertical-align: 3px; margin-right: 0.068em;"><span style="display: inline-block; width: 0px; height: 4.011em;"></span></span></span><span style="display: inline-block; width: 0px; height: 4.011em;"></span></span><span style="position: absolute; clip: rect(2.977em, 1001.04em, 4.528em, -999.997em); top: -4.069em; left: 0em;"><span><img src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS/TeX/png/Size1/Regular/336/221A.png?V=2.7.0" style="width: 16px; height: 19px; vertical-align: -6px; margin-right: 0.003em;"></span><span style="display: inline-block; width: 0px; height: 4.011em;"></span></span><span style="position: absolute; clip: rect(2.007em, 1000.26em, 2.912em, -999.997em); top: -3.035em; left: 0.391em;"><span class="mn" id="MathJax-Span-8" style="display: inline-block; font-size: 51.7%;"><span style="font-family: STIXGeneral, &quot;Arial Unicode MS&quot;, serif; font-size: 84%; font-style: normal; font-weight: normal;">۳</span></span><span style="display: inline-block; width: 0px; height: 2.589em;"></span></span></span></span></span><span style="display: inline-block; width: 0px; height: 2.589em;"></span></span></span><span style="display: inline-block; overflow: hidden; vertical-align: -0.458em; border-left: 0px solid; width: 0px; height: 1.619em;"></span></span></nobr><span class="MJX_Assistive_MathML" role="presentation"><math xmlns="http://www.w3.org/1998/Math/MathML"><mroot><mrow><mn>۲۵</mn><mo>&nbsp;</mo><mi>x</mi></mrow><mn>۳</mn></mroot></math></span></span><script type="math/mml" id="MathJax-Element-1"><math xmlns="http://www.w3.org/1998/Math/MathML"><mroot><mrow><mn>۲۵</mn><mo>&nbsp;</mo><mi>x</mi></mrow><mn>۳</mn></mroot></math></script>&nbsp;چیست ؟&nbsp;</p>
-    """;
-
     double deviceHeight = getHeight(context);
     double deviceWidth = getWidth(context);
     double textScaleFactor = getTextScaleFactor(context);
@@ -216,8 +209,10 @@ class _HomeScreenState extends State<HomeScreen> {
                           header: DateFormat.EEEE().format(DateTime.now()),
                           name: dashboardInfo.UserTraningPrograms.firstWhere(
                               (element) => element.WeekDayId == DateTime.now().weekday).ExerciseGroupName,
-                          icon: Icon(
-                            Icons.watch,
+                          iconImage: Image.asset(
+                            'assets/icons/measurement_workout.png',
+                            height: 30,
+                            width: 30,
                             color: Colors.amber,
                           ),
                           image: Image.asset(
@@ -239,8 +234,10 @@ class _HomeScreenState extends State<HomeScreen> {
                           title: 'Upcoming Meal',
                           header: DateFormat.Hm().format(DateTime.now()),
                           name:  dashboardInfo.UserDiets[DateTime.now().weekday].Time,
-                          icon: Icon(
-                            Icons.watch,
+                          iconImage: Image.asset(
+                            'assets/icons/drawer_menu_diet.png',
+                            height: 30,
+                            width: 30,
                             color: Colors.amber,
                           ),
                           image: Image.asset(
@@ -262,8 +259,10 @@ class _HomeScreenState extends State<HomeScreen> {
                           title: 'Upcoming Events',
                           header: DateFormat.MMMd().format(DateTime.now()),
                           name: 'Mon 18:00 - 20:00',
-                          icon: Icon(
-                            Icons.watch,
+                          iconImage: Image.asset(
+                            'assets/icons/measurement_weight.png',
+                            height: 30,
+                            width: 30,
                             color: Colors.amber,
                           ),
                           image: Image.asset(
